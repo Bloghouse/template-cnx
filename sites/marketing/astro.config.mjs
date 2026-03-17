9d7ee8a439e9daa666feab066e5f7b1cd498f6fe
Bloghouse <bloghouse021@gmail.com>
2026-03-17 13:29:12 -0300

    chore: remove F8 junk [CNX setup]

--- sites/marketing/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-export default defineConfig({
-  output: 'static',
-  site: 'https://marketing.vercel.app',
-  integrations: [tailwind({ applyBaseStyles: false })],
-});
