056d121c67f5cbb469f3cfc66422398c173b75f8
Bloghouse <bloghouse021@gmail.com>
2026-03-17 19:39:20 -0300

    build(template): Add React component astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  output: 'static',
   site: 'https://meu-site.vercel.app',
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind(), react(), sitemap()]
 });
