89bebce113e14a260963578c3ecb27016adc3272
Bloghouse <bloghouse021@gmail.com>
2026-03-17 13:28:28 -0300

    chore: remove F8 junk [CNX setup]

--- sites/health-coach-blog/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-export default defineConfig({
-  output: 'static',
-  site: 'https://health-coach-blog.vercel.app',
-  integrations: [tailwind({ applyBaseStyles: false })],
-});
